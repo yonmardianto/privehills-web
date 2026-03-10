c6b3cdf8e34e68c10aa66c6de4a0121fbb32c147
Yon Mardianto <ymardianto@gmail.com>
2026-03-10 23:12:30 +0700

    add fix css fontgoogleapi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,16 +33,16 @@ export default function RootLayout({
   return (
     <html lang="id" className="scroll-smooth">
       <head>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-        {/* <link
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
-        /> */}
-        {/* <link
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Jost:wght@300;400;500;600&display=swap"
           rel="stylesheet"
-        /> */}
+        />
       </head>
       <body className="grain antialiased">{children}</body>
     </html>
