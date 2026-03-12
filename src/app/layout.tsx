ab92a518469042a11e26e35a6a449daf8022494e
Yon Mardianto <ymardianto@gmail.com>
2026-03-12 16:20:41 +0700

    add viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 const BASE_URL =
@@ -23,6 +23,13 @@ export const metadata: Metadata = {
       },
     ],
   },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
